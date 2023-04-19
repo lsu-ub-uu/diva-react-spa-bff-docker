28cd7456090b8d01824469f3421cbad0d0756ef3
egiiiiil <sixteenth.bird@gmail.com>
2023-04-19 15:31:40 +0200

    .

--- buildDocker.sh
+++ buildDocker.sh
@@ -3,9 +3,9 @@
 BASENAME=${1:-'/'}
 VERSION=${2:-'latest'}
 
-TAG=diva-react-spa-bff-docker
+TAG=diva-react-spa-bff
 
-echo Building diva-react-spa-bff-docker with BASENAME $BASENAME and VERSION $VERSION
+echo Building diva-react-spa-bff with BASENAME $BASENAME and VERSION $VERSION
 
 docker build --no-cache --pull \
     --build-arg BASENAME=$BASENAME \
--- startDocker.sh
+++ startDocker.sh
@@ -3,4 +3,4 @@
 docker run --restart=unless-stopped -d\
     --name diva-react-spa-bff-dev \
     -p 9876:80 \
-    diva-react-spa-bff-docker:latest $1
+    diva-react-spa-bff:latest $1
