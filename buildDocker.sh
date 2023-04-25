5f2f3c0e35a14586712212352a4d41330f7947cd
egiiiiil <sixteenth.bird@gmail.com>
2023-04-25 15:46:48 +0200

    Added CORA_API_URL

--- buildDocker.sh
+++ buildDocker.sh
@@ -2,11 +2,13 @@
 
 BASENAME=${1:-'/'}
 VERSION=${2:-'latest'}
+CORA_API_URL=${3:-'CORA_API_URL'}
 
 TAG=diva-react-spa-bff
 
-echo Building diva-react-spa-bff with BASENAME $BASENAME and VERSION $VERSION
+echo Building diva-react-spa-bff with BASENAME $BASENAME and VERSION $VERSION and CORA_API_URL $CORA_API_URL
 
 docker build --no-cache --pull=false \
     --build-arg BASENAME=$BASENAME \
+    --build-arg CORA_API_URL=$CORA_API_URL \
     -t $TAG ./docker
